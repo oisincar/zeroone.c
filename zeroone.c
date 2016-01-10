9c8dc8c2a7555b147b6dbaa809f3b45ffe12ed20
oisincar <oisincar@gmail.com>
2016-01-10 16:07:57 +0000

    Minor tweaks

---
+++ zeroone.c
@@ -0,0 +1,18 @@
+            #include                            "stdio.h"
+    float a,b,c,d=0,e=10;f,g,                 h=0,i=0,j=0
+   ,k,l,m,n[        10000];main         (int O,char*o[]){
+  char p[10001         ];FILE*F=     fopen(__FILE__,"r");
+  int*q=O>1?o[1][      0]!=119?0:        (int[9]){79,0x69
+ ,115,   0151,0x6e     ,99,97,114           ,056}:0;while
+ (n[++      h]=O=         getc(F),          O>0)O==10?i=i
+ >g?i:       g,g=1,       j++:g++;          fclose(F);for
+ (;;){        memset(p   ,32,i*j);          d+=0.10;a=sin
+ (d);b=         cos(d);    usleep(          50000);for(c=
+ e;c>=0           ;c--){g  =k=l=0;          while(g++<h-1
+  ){O=n[           g];f=i/2+(k-i/2          )*a-(c-e/2)*b
+  +(i*l);             k=O==10?l++           ,0:O!=32?O=!c
+    ||c==e?q              ?q[g%             9]:O:64,p[f]=
+    a<0?O:p[f]==32?O:p[f],-~k:        -~k;}}g=0;while(g++<j){
+        p[g*i-1]=10;}printf           ("\r%s\x1b[%iA",p,j);}}
+ 
+ /// Ignore warnings when running. Oisin Carroll 9/1/2015 \\\
